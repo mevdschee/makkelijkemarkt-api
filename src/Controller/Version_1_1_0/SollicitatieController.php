a7fe7bebabcfa1c98e61db01ef52fb32988849e7
Maurits van der Schee <maurits@vdschee.nl>
2020-11-25 00:22:08 +0100

    refactor

--- src/Controller/Version_1_1_0/SollicitatieController.php
+++ src/Controller/Version_1_1_0/SollicitatieController.php
@@ -20,7 +20,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Method;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
