79272a8c0ae4e5a65258c630d462c87b04ee8404
Maurits van der Schee <maurits@vdschee.nl>
2020-11-25 03:26:56 +0100

    refactor

--- src/Service/PdfFactuurService.php
+++ src/Service/PdfFactuurService.php
@@ -12,163 +12,157 @@
 namespace App\Service;
 
 use Qipsius\TCPDFBundle\Controller\TCPDFController;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class PdfFactuurService
 {
-    /**
-     * @var ContainerInterface
-     */
-    protected $container;
-
-    /**
-     * @var \TCPDF $pdf
-     */
-    protected $pdf;
-
     protected $fontname;
 
     protected $fontnameBold;
 
     protected $factuurService;
 
-    public function __construct($container)
+    public function __construct(FactuurService $factuurService)
     {
-        $this->container = $container;
-        $this->factuurService = $container->get('appapi.factuurservice');
+        $this->factuurService = $factuurService;
 
-        $this->fontname = \TCPDF_FONTS::addTTFfont(
-            getcwd() . '/src/GemeenteAmsterdam/MakkelijkeMarkt/AppApiBundle/Resources/public/fonts/Avenir-Roman.ttf',
-            'TrueTypeUnicode',
-            '',
-            96
-        );
-        $this->fontnameBold = \TCPDF_FONTS::addTTFfont(
-            getcwd() . '/src/GemeenteAmsterdam/MakkelijkeMarkt/AppApiBundle/Resources/public/fonts/Avenir-Heavy.ttf',
-            'TrueTypeUnicode',
-            '',
-            96
-        );
+        $this->fontname = 'dejavusans';
+        $this->fontnameBold = 'dejavusansb';
+
+        // should we condtionally add fonts? (bundle build step)
+        // are we licensed to distribute?
+        //
+        // $this->fontname = \TCPDF_FONTS::addTTFfont(
+        //     getcwd() . '/src/GemeenteAmsterdam/MakkelijkeMarkt/AppApiBundle/Resources/public/fonts/Avenir-Roman.ttf',
+        //     'TrueTypeUnicode',
+        //     '',
+        //     96
+        // );
+        // $this->fontnameBold = \TCPDF_FONTS::addTTFfont(
+        //     getcwd() . '/src/GemeenteAmsterdam/MakkelijkeMarkt/AppApiBundle/Resources/public/fonts/Avenir-Heavy.ttf',
+        //     'TrueTypeUnicode',
+        //     '',
+        //     96
+        // );
     }
 
     public function generate(TCPDFController $tcpdf, $koopman, $dagvergunningen)
     {
-        $this->pdf = $tcpdf->create();
+        $pdf = $tcpdf->create();
 
         // set document information
-        $this->pdf->SetCreator('Gemeente Amsterdam');
-        $this->pdf->SetAuthor('Gemeente Amsterdam');
-        $this->pdf->SetTitle('Factuur');
-        $this->pdf->SetSubject('Factuur');
-        $this->pdf->SetKeywords('Factuur');
+        $pdf->SetCreator('Gemeente Amsterdam');
+        $pdf->SetAuthor('Gemeente Amsterdam');
+        $pdf->SetTitle('Factuur');
+        $pdf->SetSubject('Factuur');
+        $pdf->SetKeywords('Factuur');
 
-        $this->pdf->SetPrintHeader(false);
-        $this->pdf->SetPrintFooter(false);
-        $this->pdf->SetAutoPageBreak(false, 0);
+        $pdf->SetPrintHeader(false);
+        $pdf->SetPrintFooter(false);
+        $pdf->SetAutoPageBreak(false, 0);
 
         foreach ($dagvergunningen as $vergunning) {
             if ($vergunning->getFactuur() !== null) {
-                $this->addVergunning($koopman, $vergunning);
+                $this->addVergunning($pdf, $koopman, $vergunning);
             }
         }
 
-        return $this->pdf;
+        return $pdf;
     }
 
-    protected function addVergunning($koopman, $vergunning)
+    protected function addVergunning(\TCPDF $pdf, $koopman, $vergunning)
     {
-        $this->pdf->AddPage();
-        $this->pdf->Image(
+        $pdf->AddPage();
+        $pdf->Image(
             getcwd() . '/src/GemeenteAmsterdam/MakkelijkeMarkt/AppApiBundle/Resources/public/images/GASD_1.png',
             10,
             10,
             50
         );
 
-        $this->pdf->Ln(40);
+        $pdf->Ln(40);
 
-        $this->pdf->SetFont($this->fontname, 'b', 8);
-        $this->pdf->Cell(16, 6, '', 0, 0);
-        $this->pdf->Cell(164, 6, '', 0, 0);
+        $pdf->SetFont($this->fontname, 'b', 8);
+        $pdf->Cell(16, 6, '', 0, 0);
+        $pdf->Cell(164, 6, '', 0, 0);
 
-        $this->pdf->Ln(10);
+        $pdf->Ln(10);
 
-        $this->pdf->SetFont($this->fontname, 'b', 11);
-        $this->pdf->Cell(16, 6, '', 0, 0);
-        $this->pdf->Cell(164, 6, $koopman->getVoorletters() . ' ' . $koopman->getTussenvoegsels() . ' ' . $koopman->getAchternaam(), 0, 1);
+        $pdf->SetFont($this->fontname, 'b', 11);
+        $pdf->Cell(16, 6, '', 0, 0);
+        $pdf->Cell(164, 6, $koopman->getVoorletters() . ' ' . $koopman->getTussenvoegsels() . ' ' . $koopman->getAchternaam(), 0, 1);
 
-        $this->pdf->SetY(10);
+        $pdf->SetY(10);
 
-        $this->pdf->SetFont($this->fontname, 'b', 10);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, 'Stadswerken', 0, 0);
-        $this->pdf->Ln(5);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, 'Bezoekadres', 0, 0);
-        $this->pdf->Ln(5);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, 'Amstel 1', 0, 0);
-        $this->pdf->Ln(5);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, '1011 PN Amsterdam', 0, 0);
+        $pdf->SetFont($this->fontname, 'b', 10);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, 'Stadswerken', 0, 0);
+        $pdf->Ln(5);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, 'Bezoekadres', 0, 0);
+        $pdf->Ln(5);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, 'Amstel 1', 0, 0);
+        $pdf->Ln(5);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, '1011 PN Amsterdam', 0, 0);
 
-        $this->pdf->Ln(10);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, 'Postbus 202', 0, 0);
-        $this->pdf->Ln(5);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, '1000 AE Amsterdam', 0, 0);
-        $this->pdf->Ln(5);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, 'Telefoon 020 2552912', 0, 0);
-        $this->pdf->Ln(5);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, 'Bereikbaar van 8.00-18.00', 0, 0);
-        $this->pdf->Ln(5);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, 'Email', 0, 0);
-        $this->pdf->Ln(5);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, 'debiteurenadministratie@amsterdam.nl', 0, 0);
+        $pdf->Ln(10);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, 'Postbus 202', 0, 0);
+        $pdf->Ln(5);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, '1000 AE Amsterdam', 0, 0);
+        $pdf->Ln(5);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, 'Telefoon 020 2552912', 0, 0);
+        $pdf->Ln(5);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, 'Bereikbaar van 8.00-18.00', 0, 0);
+        $pdf->Ln(5);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, 'Email', 0, 0);
+        $pdf->Ln(5);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, 'debiteurenadministratie@amsterdam.nl', 0, 0);
 
-        $this->pdf->Ln(5);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, 'BTW nr NL002564440B01', 0, 0);
-        $this->pdf->Ln(5);
-        $this->pdf->Cell(130, 6, '', 0, 0);
-        $this->pdf->Cell(50, 6, 'KvK nr 34366966 0000', 0, 0);
+        $pdf->Ln(5);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, 'BTW nr NL002564440B01', 0, 0);
+        $pdf->Ln(5);
+        $pdf->Cell(130, 6, '', 0, 0);
+        $pdf->Cell(50, 6, 'KvK nr 34366966 0000', 0, 0);
 
-        $this->pdf->Ln(10);
+        $pdf->Ln(10);
 
-        $this->pdf->Cell(16, 6, '', 0, 0);
-        $this->pdf->SetFont($this->fontnameBold, 'b', 9);
-        $this->pdf->Cell(26, 6, 'Factuurnummer', 0, 0);
-        $this->pdf->SetFont($this->fontname, 'b', 9);
-        $this->pdf->Cell(26, 6, 'mm' . $vergunning->getFactuur()->getId(), 0, 0);
-        $this->pdf->SetFont($this->fontnameBold, 'b', 9);
-        $this->pdf->Cell(26, 6, 'Factuurdatum', 0, 0);
-        $this->pdf->SetFont($this->fontname, 'b', 9);
+        $pdf->Cell(16, 6, '', 0, 0);
+        $pdf->SetFont($this->fontnameBold, 'b', 9);
+        $pdf->Cell(26, 6, 'Factuurnummer', 0, 0);
+        $pdf->SetFont($this->fontname, 'b', 9);
+        $pdf->Cell(26, 6, 'mm' . $vergunning->getFactuur()->getId(), 0, 0);
+        $pdf->SetFont($this->fontnameBold, 'b', 9);
+        $pdf->Cell(26, 6, 'Factuurdatum', 0, 0);
+        $pdf->SetFont($this->fontname, 'b', 9);
         $dag = implode('-', array_reverse(explode('-', $vergunning->getDag()->format('d-m-Y'))));
-        $this->pdf->Cell(26, 6, $dag, 0, 1);
+        $pdf->Cell(26, 6, $dag, 0, 1);
 
-        $this->pdf->Cell(16, 6, '', 0, 0);
-        $this->pdf->SetFont($this->fontnameBold, 'b', 9);
-        $this->pdf->Cell(144, 6, 'Omschrijving', 'B', 0);
-        $this->pdf->Cell(20, 6, 'Bedrag €', 'B', 1, 'R');
+        $pdf->Cell(16, 6, '', 0, 0);
+        $pdf->SetFont($this->fontnameBold, 'b', 9);
+        $pdf->Cell(144, 6, 'Omschrijving', 'B', 0);
+        $pdf->Cell(20, 6, 'Bedrag €', 'B', 1, 'R');
 
-        $this->pdf->SetFont($this->fontname, 'b', 9);
+        $pdf->SetFont($this->fontname, 'b', 9);
 
-        $this->pdf->Cell(16, 6, '', 0, 0);
-        $this->pdf->Cell(164, 6, 'Markt: ' . $vergunning->getMarkt()->getNaam(), '', 1);
+        $pdf->Cell(16, 6, '', 0, 0);
+        $pdf->Cell(164, 6, 'Markt: ' . $vergunning->getMarkt()->getNaam(), '', 1);
 
         $btwTotaal = [];
         $btwOver = [];
 
         foreach ($vergunning->getFactuur()->getProducten() as $product) {
-            $this->pdf->Cell(16, 6, '', 0, 0);
+            $pdf->Cell(16, 6, '', 0, 0);
             $btwText = $product->getBtwHoog() > 0 ? '. excl. ' . $product->getBtwHoog() . '% BTW' : '';
-            $this->pdf->Cell(144, 6, $product->getAantal() . ' maal ' . $product->getNaam() . $btwText, '', 0);
-            $this->pdf->Cell(20, 6, number_format($product->getAantal() * $product->getBedrag(), 2), 0, 0, 'R');
+            $pdf->Cell(144, 6, $product->getAantal() . ' maal ' . $product->getNaam() . $btwText, '', 0);
+            $pdf->Cell(20, 6, number_format($product->getAantal() * $product->getBedrag(), 2), 0, 0, 'R');
             if (!isset($btwTotaal[$product->getBtwHoog()])) {
                 $btwTotaal[$product->getBtwHoog()] = 0;
                 $btwOver[$product->getBtwHoog()] = 0;
@@ -177,26 +171,26 @@ class PdfFactuurService
             $btwTotaal[$product->getBtwHoog()] += number_format($product->getAantal() * $product->getBedrag() * ($product->getBtwHoog() / 100), 2);
             $btwOver[$product->getBtwHoog()] += number_format($product->getAantal() * $product->getBedrag(), 2);
 
-            $this->pdf->Ln(5);
+            $pdf->Ln(5);
         }
 
-        $this->pdf->Ln(5);
+        $pdf->Ln(5);
 
-        $this->pdf->Cell(98, 6, '', 0, 0);
-        $this->pdf->Cell(41, 6, 'Subtotaal', 'T', 0);
-        $this->pdf->Cell(41, 6, $this->factuurService->getTotaal($vergunning->getFactuur(), false), 'T', 0, 'R');
-        $this->pdf->Ln(5);
+        $pdf->Cell(98, 6, '', 0, 0);
+        $pdf->Cell(41, 6, 'Subtotaal', 'T', 0);
+        $pdf->Cell(41, 6, $this->factuurService->getTotaal($vergunning->getFactuur(), false), 'T', 0, 'R');
+        $pdf->Ln(5);
         foreach ($btwTotaal as $key => $value) {
-            $this->pdf->Cell(98, 6, '', 0, 0);
-            $this->pdf->Cell(41, 6, 'BTW ' . $key . '% over ' . number_format($btwOver[$key], 2), 0, 0);
-            $this->pdf->Cell(41, 6, number_format($value, 2), 0, 0, 'R');
-            $this->pdf->Ln(5);
+            $pdf->Cell(98, 6, '', 0, 0);
+            $pdf->Cell(41, 6, 'BTW ' . $key . '% over ' . number_format($btwOver[$key], 2), 0, 0);
+            $pdf->Cell(41, 6, number_format($value, 2), 0, 0, 'R');
+            $pdf->Ln(5);
         }
 
-        $this->pdf->SetFont($this->fontnameBold, 'b', 9);
-        $this->pdf->Cell(98, 6, '', 0, 0);
-        $this->pdf->Cell(41, 6, 'Totaal', 'T', 0);
-        $this->pdf->Cell(41, 6, $this->factuurService->getTotaal($vergunning->getFactuur()), 'T', 0, 'R');
+        $pdf->SetFont($this->fontnameBold, 'b', 9);
+        $pdf->Cell(98, 6, '', 0, 0);
+        $pdf->Cell(41, 6, 'Totaal', 'T', 0);
+        $pdf->Cell(41, 6, $this->factuurService->getTotaal($vergunning->getFactuur()), 'T', 0, 'R');
 
     }
 }
