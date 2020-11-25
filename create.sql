9a3398e3569388be6e82bd59d7b324776e629eeb
Maurits van der Schee <maurits@vdschee.nl>
2020-11-25 02:15:28 +0100

    refactor

--- create.sql
+++ create.sql
@@ -1,4 +1,3 @@
-CREATE DATABASE `makkelijkemarkt` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER 'makkelijkemarkt'@'localhost' IDENTIFIED BY 'makkelijkemarkt';
-GRANT ALL PRIVILEGES ON `makkelijkemarkt`.* TO 'makkelijkemarkt'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+CREATE USER "makkelijkemarkt" WITH PASSWORD 'makkelijkemarkt';
+CREATE DATABASE "makkelijkemarkt";
+GRANT ALL PRIVILEGES ON DATABASE "makkelijkemarkt" to "makkelijkemarkt";
