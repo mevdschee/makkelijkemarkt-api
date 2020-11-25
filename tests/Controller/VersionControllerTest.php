5195346185dccda43f12592a2ceebb8c7d76e888
Maurits van der Schee <maurits@vdschee.nl>
2020-11-25 01:52:25 +0100

    refactor

--- tests/Controller/VersionControllerTest.php
+++ tests/Controller/VersionControllerTest.php
@@ -11,8 +11,8 @@ class VersionControllerTest extends WebTestCase
         $client->request('GET', '/1.1.0/version/');
         $response = $client->getResponse();
 
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertTrue($response->headers->contains('Content-Type', 'application/json'));
-        $this->assertEquals('1.1.0', json_decode($response->getContent(), true)['apiVersion']);
+        $this->assertEquals(200, $response->getStatusCode(), 'Status code is 200');
+        $this->assertTrue($response->headers->contains('Content-Type', 'application/json'), 'Content type is JSON');
+        $this->assertEquals('1.1.0', json_decode($response->getContent(), true)['apiVersion'], 'API version is 1.1.0');
     }
 }
