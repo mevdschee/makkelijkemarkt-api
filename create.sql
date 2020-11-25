2beef0c25cd72c75798d25043a84d5fac6bc04aa
Maurits van der Schee <maurits@vdschee.nl>
2020-11-25 03:40:05 +0100

    refactor

--- create.sql
+++ create.sql
@@ -1,3 +1,4 @@
 CREATE USER "makkelijkemarkt" WITH PASSWORD 'makkelijkemarkt';
 CREATE DATABASE "makkelijkemarkt";
 GRANT ALL PRIVILEGES ON DATABASE "makkelijkemarkt" to "makkelijkemarkt";
+CREATE EXTENSION "uuid-ossp";
