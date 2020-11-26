abb8f018048ad944ac11f96faf3264f02d8a0077
Maurits van der Schee <maurits@vdschee.nl>
2020-11-26 01:59:26 +0100

    fixtures

--- src/DataFixtures/MarktData.php
+++ src/DataFixtures/MarktData.php
@@ -23,23 +23,6 @@ class MarktData extends Fixture
         $soorten = ['dag', 'week', 'seizoen'];
         $aanwezigeOpties = ["3mKramen", "4mKramen", "extraMeters", "elektra", "afvaleiland"];
 
-        //        COPY public.markt (id, naam, geo_area, afkorting, soort, perfect_view_nummer, markt_dagen, standaard_kraam_afmeting, extra_meters_mogelijk, aanwezige_opties, aantal_kramen, aantal_me
-        //        ter, audit_max, kies_je_kraam_mededeling_actief, kies_je_kraam_mededeling_titel, kies_je_kraam_mededeling_tekst, kies_je_kraam_actief, kies_je_kraam_fase, markt_dagen_tekst, indeling
-        //        s_tijdstip_tekst, telefoon_nummer_contact, makkelijke_markt_actief, kies_je_kraam_geblokkeerde_plaatsen, kies_je_kraam_geblokkeerde_data, kies_je_kraam_email_kramenzetter, indelingst
-        //        ype) FROM stdin;
-        //        id  naam      geo afkorting soort  pfn mkt_dgn, stdafm
-        //        46    TestMarkt    \N    BWMMZ    seizoen    38    \N    3    f    ["3mKramen","4mKramen"]    \N    \N    10    f    \N    \N    f\N    \N    \N    \N    t    \N    \N    \N    traditioneel
-        //        47    TestMarkt    \N    BWMZF    seizoen    39    \N    3    f    ["3mKramen","4mKramen"]    \N    \N    10    f    \N    \N    f    \N    \N    \N    \N    t    \N    \N    \N    traditioneel
-        //        44    TestMarkt    \N    JMAMS    seizoen    36    \N    3    f    ["3mKramen","4mKramen"]    \N    \N    10    f    \N    \N    f    \N    \N    \N    \N    t    \N    \N    \N    traditioneel
-        //        41    TestMarkt    \N    JM-MZ    seizoen    33    \N    3    f    ["3mKramen","4mKramen"]    \N    \N    10    f    \N    \N    f    \N    \N    \N    \N    t    \N    \N    \N    traditioneel
-        //        42    TestMarkt    \N    JM-ZF    seizoen    34    \N    3    f    ["3mKramen","4mKramen"]    \N    \N    10    f    \N    \N    f    \N    \N    \N    \N    t    \N    \N    \N    traditioneel
-        //        48    TestMarkt    \N    GEEN    seizoen    41    \N     0    f    \N    \N    \N    10    f    \N    \N    f    \N    \N    \N    \N    t    \N    \N    \N    traditioneel
-        //        56    TestMarkt    \N    VENT    dag        \N    \N     0    f    []    \N    \N    10    f    \N    \N    f    \N    \N    \N    \N    t    \N    \N    \N    traditioneel
-        //        52    TestMarkt    \N    KR      week    46    \N        3    t    ["3mKramen","4mKramen","extraMeters","elektra"]    82    328    10    f    \N    \N    f    \N    \N    \N    \N    t    \N    \N    \N    traditioneel
-        //        55    TestMarkt    \N    STPL    week    \N    \N        0    f    []    \N    \N    10    f    \N    \N    f    \N    \N    \N    \N    t    \N    \N    \N    traditioneel
-        //        31    TestMarkt    \N    NOM-Z    week    15    ma,za    3    t    ["3mKramen","4mKramen","extraMeters","elektra","afvaleiland"]    64    512    10    f    \N    \N    f    \N    \N    \N    \N    t    \N    \N    \N    traditioneel
-        //
-
         for ($i = 0; $i < 14; $i++) {
             $afkorting = "TM-$i";
             $nummer = $fib100($i + 10);
