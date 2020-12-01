26bf57cea0581ae1f3d94e5f8a792f32ffa12622
Maurits van der Schee <maurits@vdschee.nl>
2020-12-01 21:17:18 +0100

    fix logout bug

--- tests/Controller/LoginControllerTest.php
+++ tests/Controller/LoginControllerTest.php
@@ -153,4 +153,34 @@ class LoginControllerTest extends WebTestCase
         $this->assertEquals(['error' => 'Invalid token time'], $result);
     }
 
+    public function testBasicId()
+    {
+        $client = static::createClient();
+
+        // first request
+        $client->request('POST', '/api/1.1.0/login/basicUsername/', [], [], [
+            'HTTP_MmAppKey' => 'testkey',
+        ], json_encode([
+            'username' => "account1@amsterdam.nl",
+            'password' => "Password1!",
+        ]));
+        $response = $client->getResponse();
+        $result = json_decode($response->getContent(), true);
+        $this->assertTrue($response->isSuccessful(), 'Request has succeeded');
+        $this->assertEquals([], $result);
+        $uuid = $result['uuid'];
+
+        // second request
+        $client->request('POST', '/api/1.1.0/login/basicId/', [], [], [
+            'HTTP_MmAppKey' => 'testkey',
+        ], json_encode([
+            'username' => "account1@amsterdam.nl",
+            'password' => "Password1!",
+        ]));
+        $response = $client->getResponse();
+        $result = json_decode($response->getContent(), true);
+        $this->assertTrue($response->isSuccessful(), 'Request has succeeded');
+        $this->assertEquals('account1@amsterdam.nl', $result['account']['username']);
+        $uuid = $result['uuid'];
+    }
 }
